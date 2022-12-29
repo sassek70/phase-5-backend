633a28752043404c9025faa0dc219a49cde4aac2
Kevin Sasse <sassek70@gmail.com>
2022-12-29 14:30:11 -0500

    game logical finished - I think

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -6,9 +6,10 @@ class GamesController < ApplicationController
     def create_game
         new_game = Game.create!(host_user_id: params[:id])
         user = User.find(params[:id])
-        increase_games = user.gamesPlayed + 1
-        user.update!(gamesPlayed: increase_games)
+        # increase_games = user.gamesPlayed + 1
+        user.update!(gamesPlayed: user.gamesPlayed + 1)
         # debugger
+        # user.update!(gamesPlayed: user.gamesPlayed += 1)
         # user.update_attribute(:gamesPlayed, increase_games)
         render json: new_game, status: :ok
     end
@@ -17,7 +18,7 @@ class GamesController < ApplicationController
         game = Game.find_by!(game_key: params[:game_key])
         winner = User.find(params[:winning_player_user_id])
         game.update!(winning_player_id: winner.id)
-        # winner.update!(game_won: user.games_won += 1)
+        winner.update!(gamesWon: winner.gamesWon + 1)
     end
 
     def join_game
@@ -27,8 +28,8 @@ class GamesController < ApplicationController
             return render json: {error: "Game is full" }, status: :unprocessable_entity
         else 
             game.update!(opponent_id: params[:opponent_id])
-            # user.update!(gamesPlayed: user.gamesPlayed += 1)
-            GameSessionChannel.broadcast_to game, {action: "user-joined", game: game}
+            user.update!(gamesPlayed: user.gamesPlayed + 1)
+            GameSessionChannel.broadcast_to game, {action: "user-joined", game: game, message: "Opponent has joined the game"}
              render json: game, status: :accepted
         end
     end
