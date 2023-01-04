d5c35ac661dbfa76a91f6192f6b8eca4a754a6ef
Kevin Sasse <sassek70@gmail.com>
2023-01-04 13:26:30 -0500

    set default deck size

--- app/controllers/user_cards_controller.rb
+++ app/controllers/user_cards_controller.rb
@@ -13,7 +13,7 @@ class UserCardsController < ApplicationController
     def create_random_deck
         game = Game.find(params[:game_id])
         random_deck = []
-        6.times do
+        30.times do
             card = Card.all.sample
             player_card = UserCard.create!(user_id: params[:player_id], card_id: card.id, game_id: params[:game_id])
             random_deck << player_card
