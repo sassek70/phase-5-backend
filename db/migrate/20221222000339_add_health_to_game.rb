fdf0b4e0aac844b85ba613a04ec76e11ba022049
Kevin Sasse <sassek70@gmail.com>
2023-01-04 13:26:14 -0500

    added user delete. Set default deck size and health

--- db/migrate/20221222000339_add_health_to_game.rb
+++ db/migrate/20221222000339_add_health_to_game.rb
@@ -1,6 +1,6 @@
 class AddHealthToGame < ActiveRecord::Migration[7.0]
   def change
-    add_column :games, :host_player_health, :integer, default: 15
-    add_column :games, :opponent_player_health, :integer, default: 15
+    add_column :games, :host_player_health, :integer, default: 20
+    add_column :games, :opponent_player_health, :integer, default: 20
   end
 end
