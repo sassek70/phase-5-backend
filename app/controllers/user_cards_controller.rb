1cba6b18ec2e64269048741ea57cc56bfb2beed3
Kevin Sasse <sassek70@gmail.com>
2023-01-24 17:16:00 -0500

    fixed alert issues

--- app/controllers/user_cards_controller.rb
+++ app/controllers/user_cards_controller.rb
@@ -13,11 +13,12 @@ class UserCardsController < ApplicationController
     def create_random_deck
         game = Game.find(params[:game_id])
         random_deck = []
-        1.times do
-            card = Card.first
-            # card = Card.all.sample
+        30.times do
+            # card = Card.first
+            card = Card.all.sample
             player_card = UserCard.create!(user_id: params[:player_id], card_id: card.id, game_id: params[:game_id])
             random_deck << player_card
+
         end
         game_cards = UserCard.where("game_id = ?", game.id)
         GameSessionChannel.broadcast_to game, {action: "all-cards", game_cards: game_cards.map{|game_card| UserCardSerializer.new(game_card)}, card_map: get_card_map(game_cards)}
