7b5a79cc14c6d97ee45dd421a3180cad8e1206c1
Kevin Sasse <sassek70@gmail.com>
2023-01-26 11:48:57 -0500

    created build script

--- bin/ANSMG-build.sh
+++ bin/ANSMG-build.sh
@@ -3,9 +3,9 @@
      set -o errexit
 
 # front end build commands 
-rm -rf public
-npm install --prefix client && npm run build --prefix client
-cp -a client/build/. public/
+# rm -rf public
+# npm install --prefix client && npm run build --prefix client
+# cp -a client/build/. public/
 # backend build commands
 bundle install
 bundle exec rake db:migrate db:seed:replant
