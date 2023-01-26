d3806ac5ae1961050ebd6230822e72db097f6723
Kevin Sasse <sassek70@gmail.com>
2023-01-26 14:41:32 -0500

    update production file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     #change this to front end url for deployment
-    origins "*"
+    origins "https://a-not-so-magical-gathering.onrender.com"
 
     resource "*",
       headers: :any,
